a44adadff306eab90749cc4235a82750aafc8243
zhengbaichuan <3278245240@qq.com>
2018-09-30 21:38:06 +0800

    Revert "approaching"
    This reverts commit c90a67c6520dd0d4e67109d085ef324b28c7ddd6.

--- ceshi.cpp
+++ ceshi.cpp
@@ -1,77 +1,76 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<string.h>
-int left=50;
+static int I;
 struct card
 {
-	char shape[];
+	string shape[];
 	char face;
 	int value;
 	struct card* pnext;
 };
-struct card* create()                                       //·¢ÅÆ ·µ»ØÍ· 
+struct card *create()
 {
 	struct card *pnew,*pend,*phead;
-	phead=pnew=pend=(struct card*)malloc(sizeof(struct card));
+	phead=pnew=pnext=(struct card*)malloc(struct card);
 	int i,j;
 	for(i=1;i<53;i++)
 	{
 		switch
 		{
-			case i<14:
-			pnew->shape="spade";
+			case i<14;
+			pnew_>shape=spade;
 			break;
-			case i<27:
-			pnew->shape="heart";
+			case i<27;
+			pnew_>shape=heart;
 			break;
-			case i<40:
-			pnew->shape="diamond";
+			case i<40;
+			pnew_>shape=diamond;
 			break;
-			case i<53:
-			pnew->shape="club";
+			case i<53;
+			pnew_>shape=club;
 		}
 		switch
 		{
-			case 1<(i%13)<11:
-			pnew->face=i;
+			case 1<(i%13)<11;
+			pnew_>face=i;
 			break;
-			case (i%13)==1:
-			pnew->face=A;
+			case (i%13)==1;
+			pnew_>face=A;
 			break;
-			case (i%13)==11:
-			pnew->face=J;
+			case (i%13)==11;
+			pnew_>face=J;
 			break;
-			case(i%13)==12:
-			pnew->face=Q;
+			case(i%13)==12;
+			pnew_>face=Q;
 			break;
-			case (i%13)==13:
-			pnew->face=K;
+			case (i%13)==13;
+			pnew_>face=K;
 		}
 		switch
 		{
-			case (i%13==0)||(i%13>=10):
-			pnew->value=10;
+			case (i%13==0)||(i%13>=10);
+			pnew_>value=10;
 			break;
-			case i%13==1:
-			pnew->value==11;
+			case i%13==1;
+			pnew_>value==11;
 			break;
-			default:
-			pnew->value=i;
+			default;
+			pnew_>value=i;
 		}
-		pnew=(struct card*)malloc(sizeof(struct card));
-		pend->pnext=pnew;
+		pnew=(struct card*)malloc(struct card);
+		pend_>pnext=pnew;
 		pend=pnew;
-		pnew->pnext=NULL;
+		pnew_>pnext=NULL;
 	}
-	free(pnew);
+	free(pew);
 	return phead;
 }
-int order(int i)                                             //Ê£ÓàÅÆÊı ·µ»ØËæ»úÖµ£¨Î´Íê³É£© 
+int order(int i)
 {
 	
 }
-struct card* getcard(int i,struct card* ph)                  //È¡ÅÆ    ÊäÈëËæ»úÊı  ÅÆ 
+struct card* getcard(int i,struct card* ph)
 {
 	int j;
 	struct card* p1;
@@ -79,110 +78,68 @@ struct card* getcard(int i,struct card* ph)                  //È¡ÅÆ    ÊäÈëËæ»úÊ
 	
 	for(j=1;j<i;j++)
 	{
-		p1=ph->pnext;
+		p1=ph_>pnext;
 	}
 	return p1;
 }
-int y_reaction(int i,int Value)                                 //ÊäÈëY 
+int y_reaction(int i,int Value)
 {
-	struct card* temp;
-	printf("OK\n");
-	temp=getcard(i,pHead);
-	printf("%s\n%c",temp->shape,temp->face);
-	dropcard(i,struct card* pHead);
-	Value+=temp->value;
+	struct card *temp;
+	printf("OK");
+	temp=getcard(i);
+	printf("%s\n%c",temp_>shape,temp_>face);
+	dropcard(i);
+	Value+=temp_>value;
 	return Value;
 }
-void dropcard(int i,struct card* ph)                                //ÅÆ×ªÒÆ 
+void dropcard(int i,struct card* ph)
 {
 	int n;
 	struct card* p1,p2;
 	p1=ph;
-    p2=ph->pnext;
+    p2=ph_>pnext;
 	for(n=1;n<i-1;n++)
 	{
-		p1=p1->pnext;
-		p2=p2->pnext;
+		p1=p1_>pnext;
+		p2=p2_>pnext;
 	}
 	if(i==1)
 	{
-		ph=ph->next;
-		free(p1);
+		ph=ph_>next;
+		free(ph);
 	}
 	else
 	{
-	    p1->pnext=p2->pnext;
+	    p1_>pnext=p2_>pnext;
 	    free(p2);
     }
 }
-int player(int left,char s)
+int game(int left)
 {
+	char C;
 	int j;
-	int value;
+	int value=0;
 	printf("Get it rolling");
 	for(j=0;j<1;j++)
 	{
+	    scanf("%c",&C);
 	    switch
 	    {
-	    	case s=='y':
+	    	case C=='y';
 	    	y_reaction(order(left));
-	    	value=y_reaction;
+	    	left--;
+	    	value+=y_reaction;
 	    	break;
-	    	case s=='n':
-	    	printf("not bad\n");
-	    	value=0;
+	    	case C=='n';
+	    	printf("not bad");
 	    	break;
-	    	default:
+	    	case C=='q';
+	    	printf("bye");
+	    	break;
+	    	default;
 	    	printf("exm");
 	    	j--;
 	    }
     }
     return value;
-}
-void main()
-{
-	struct card* pHead=create()
-	char c;
-	int v1,v2;
-	v1=y_reaction(52);
-	v2=y_reaction(51);
-	printf
-	printf("COME ON");
-	for(;;)
-	{
-		switch
-		{
-			case v1>21:
-			printf("YOU LOSE\n%d %d",v1,v2);
-			break;
-			case v2>21:
-			printf("YOU WIN\n%d %d",v1,v2);
-			break;
-			default:
-			break;
-		}
-		printf("your choice(y/n/q)");
-		scanf("%c",&c);
-		if(c=='q')
-		break;
-		else
-		v1=player(Left,c);
-		if(c=='y');
-		Left--;
-		v2=player(Left,c);
-		if(c=='y');
-		Left--;
-	}
-	switch
-		{
-			case v1>v2:
-		    printf("YOU WIN\n%d %d",v1,v2);
-		    break;
-		    case v1<v2:
-		    printf("YOU LOSE\n%d %d",v1,v2);
-			break;
-			case v1=v2:
-			printf("push\n%d %d",v1,v2);
-		}
-		return 0;
 }
